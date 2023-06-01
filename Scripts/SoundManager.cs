8c349e142c25a60d9e61e2e34306da1138e26660
Arntorius <84930941+arntorius@users.noreply.github.com>
2023-06-01 21:10:58 +0200

    1

---
+++ Scripts/SoundManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public Button muteButton;
+    public Button unmuteButton;
+
+    private float previousVolume;
+    private bool isFirstStartup = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 0.5f);
+            Load();
+            isFirstStartup = true; // Set isFirstStartup to true only during the first startup
+        }
+        else
+        {
+            Load();
+            isFirstStartup = false; // Set isFirstStartup to false if the value already exists in PlayerPrefs
+        }
+
+        volumeSlider.onValueChanged.AddListener(VolumeSliderChanged);
+        muteButton.onClick.AddListener(Mute);
+        unmuteButton.onClick.AddListener(Unmute);
+
+        UpdateMuteUnmuteButtons();
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+        UpdateMuteUnmuteButtons();
+    }
+
+    public void VolumeSliderChanged(float value)
+    {
+        UpdateMuteUnmuteButtons();
+    }
+
+    public void Mute()
+    {
+        previousVolume = volumeSlider.value;
+        volumeSlider.value = 0;
+        AudioListener.volume = 0;
+        Save();
+        UpdateMuteUnmuteButtons();
+    }
+
+    public void Unmute()
+    {
+        volumeSlider.value = previousVolume;
+        AudioListener.volume = previousVolume;
+        Save();
+        UpdateMuteUnmuteButtons();
+    }
+
+    private void Load()
+    {
+        float savedVolume = PlayerPrefs.GetFloat("musicVolume");
+        volumeSlider.value = savedVolume;
+        AudioListener.volume = savedVolume;
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+
+    private void UpdateMuteUnmuteButtons()
+    {
+        bool isMuted = volumeSlider.value <= 0;
+        muteButton.gameObject.SetActive(!isMuted);
+        unmuteButton.gameObject.SetActive(isMuted);
+
+        // Set slider to 0.5 on first startup
+        if (isFirstStartup)
+        {
+            volumeSlider.value = 0.5f;
+            AudioListener.volume = 0.5f;
+            Save();
+            isFirstStartup = false; // Reset isFirstStartup after setting the default value
+        }
+    }
+}
